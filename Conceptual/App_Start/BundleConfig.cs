9ca23a005bedbd16f46f81123ebc880bcfcf994c
Neko3000 <Neko3000@live.com>
2017-02-12 03:29:02 +0900

    built article index

--- Conceptual/App_Start/BundleConfig.cs
+++ Conceptual/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace Conceptual
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.min.css",
                       "~/Content/stylized.min.css"));
         }
     }
