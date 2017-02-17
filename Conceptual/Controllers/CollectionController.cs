633a2042b51118e334b6ed43bd99108ba8e939d1
Neko3000 <Neko3000@live.com>
2017-02-17 15:30:19 +0900

    built collection page

---
+++ Conceptual/Controllers/CollectionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Conceptual.Controllers
+{
+    public class CollectionController : Controller
+    {
+        // GET: Collection
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
